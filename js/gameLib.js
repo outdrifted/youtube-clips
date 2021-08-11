80746937f82be495dfe2794915323719a11da903
Outdrifted <diglysv@gmail.com>
2021-08-11 19:11:40 +0300

    Update gameLib.js

--- js/gameLib.js
+++ js/gameLib.js
@@ -1,11 +1,11 @@
 const gameLib = {
 	"Rocket League": {
-		"aliases": ["rl"],
+		"aliases": ["rl", "rocket league"],
 		"link": "https://www.epicgames.com/store/en-US/p/rocket-league",
 		"icon": "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/252950/9ad6dd3d173523354385955b5fb2af87639c4163.jpg"
 	},
 	"Deep Rock Galactic": {
-		"aliases": ["drg"],
+		"aliases": ["drg", "deep rock galactic"],
 		"link": "https://store.steampowered.com/app/548430/Deep_Rock_Galactic/",
 		"icon": "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/548430/e033e23c29a192a17c16a7645a2b423ac64ff447.jpg"
 	},
@@ -15,7 +15,7 @@ const gameLib = {
 		"icon": "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/730/69f7ebe2735c366c65c0b33dae00e12dc40edbe4.jpg"
 	},
 	"Sea of Thieves": {
-		"aliases": ["sot"],
+		"aliases": ["sot", "sea of thieves"],
 		"link": "https://store.steampowered.com/app/1172620/Sea_of_Thieves/",
 		"icon": "https://cdn.akamai.steamstatic.com/steamcommunity/public/images/apps/1172620/f95f362708fc326511c5d86566c447ee625bf776.jpg"
 	},
