57b1a5dc650e690159d08ce7f451d3de3743d72f
Outdrifted <diglysv@gmail.com>
2021-09-05 19:47:19 +0300

    Update playlists.js

--- js/playlists.js
+++ js/playlists.js
@@ -1,10 +1,12 @@
 const playlists = {
 	youtube: [
-		"PLeRA6x39PellI87s8Qh6s8ETDlpzsrAI0", /*yummy*/
-		"PLCgbW6VYTCweMA4Sqhd3xdAjGqrQFBrF9" /*avokenn*/
+		"PLeRA6x39PellI87s8Qh6s8ETDlpzsrAI0",	/*yummy*/
+// 		"PLCgbW6VYTCweMA4Sqhd3xdAjGqrQFBrF9"	/*avokenn*/
 	],
 	medal: [
-		13754957,  /*boynewtoy*/
-		3832163  /*garklon*/
+// 		13754957,	/*boynewtoy*/
+// 		3832163,	/*garklon*/
+// 		10191625,	/*jungy*/
+// 		3965093		/*yummy*/
 	]
 }
