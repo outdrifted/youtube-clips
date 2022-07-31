3c3c780b6307bff9e77771b138134ebffcfdae30
Vilius <diglysv@gmail.com>
2022-07-31 22:25:56 +0300

    Update overrides.js

--- js/overrides.js
+++ js/overrides.js
@@ -10,5 +10,21 @@ const overrides = { // Override data on certain clips.
         people: ['jungy', 'GarklOn', 'yummy', 'Rokas', 'Obuolių sultys'],
         game: 'CS:GO',
         dateRecorded: "2022-06-10"
+    },
+    "QUPopBp2iYk": {
+        name: "kas ce pareina #2",
+        people: ['jungy', 'GarklOn', 'yummy'],
+        game: 'RL',
+        dateRecorded: "2021-06-22"
+    },
+    "OIEuLLC2SmQ": {
+        people: ['jungy', 'GarklOn', 'yummy'],
+        game: 'RL',
+        dateRecorded: "2021-06-21"
+    },
+    "vy9iZZ9FmLo": {
+        people: ['jungy', 'GarklOn', 'yummy'],
+        game: 'RL',
+        dateRecorded: "2021-06-21"
     }
 }
